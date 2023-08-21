b5d71d28495502cc81db08779d45f1f1464647c3
Elizaveta Biryukova <birliza@mail.ru>
2023-08-21 23:00:32 +0200

    feat: add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import styles from '../../styles/Header.module.css'
+import { Link } from 'react-router-dom'
+import { ROUTES } from '../../utils/routes'
+import LOGO from '../../images/logo.svg'
+import AVATAR from '../../images/avatar.jpg'
 
 function Header() {
     return (
-        <div>Header</div>
+        <div className={styles.header}>
+            <div className={styles.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={LOGO} alt="stuff" />
+                </Link>
+            </div>
+            <div className={styles.info}>
+                <div className={styles.user}>
+                    <div className={styles.avatar} style={{ backgroundImage: `url(${AVATAR})` }} />
+                    <div className={styles.username}>Guest</div>
+                </div>
+
+                <form className={styles.form}>
+                    <div className={styles.icon}>
+                        <svg className='icon'>
+                            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`} />
+                        </svg>
+                    </div>
+                    <div className={styles.input}></div>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Routes } from ' resct-router-dom'
-import { Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from '../Home/Home'
 
 const AppRoutes = () => {
