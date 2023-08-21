76c023bd3c551a526a40d6e36ab5859a7987db0f
Elizaveta Biryukova <birliza@mail.ru>
2023-08-21 20:40:37 +0200

    feat: add project

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppRoutes from './Routes/Routs'
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppRoutes />
+        </div>
+    );
+};
+
+export default App
---
+++ src/components/Routes/Routs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes } from ' resct-router-dom'
+import { Route } from 'react-router-dom'
+import Home from '../Home/Home'
+
+const AppRoutes = () => {
+
+    <Routes>
+        <Route index element={<Home />} />
+    </Routes>
+
+}
+
+export default AppRoutes
