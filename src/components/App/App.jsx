a66d2961c00d9e9d08e46209cda5b144b7cfb643
Elizaveta Biryukova <birliza@mail.ru>
2023-08-22 13:16:18 +0200

    feat: add sidebar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import AppRoutes from '../Routes/Routes'
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Sidebar from '../Sidebar/Sidebar';
 
 const App = () => {
     return (
         <div className="app">
             <Header />
+            <Sidebar />
             <AppRoutes />
             <Footer />
         </div>
