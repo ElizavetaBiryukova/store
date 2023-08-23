b2afb27b7cd62fb51c1cf500a4bbe774126aaf95
Elizaveta Biryukova <birliza@mail.ru>
2023-08-23 16:13:35 +0200

    feat: add useSelector

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import styles from "../../styles/Sidebar.module.css";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+
+    const { list } = useSelector(({ categories }) => categories)
 
     return (
         <section className={styles.sidebar}>
             <div className={styles.title}>CATEGORIES</div>
             <nav>
                 <ul className={styles.menu}>
+                    {list.map(({ id, name }) => (
+                        <li key={id}>
+                            <NavLink
+                                className={({ isActive }) => `${styles.link} ${isActive ? styles.active : ""}`}
+                                to={`/categories/${id}`}>
+                                {name}
+                            </NavLink>
+                        </li>
+                    ))}
 
-                    <li >
-                        <NavLink to={`/categories/${1}`}>
-                            Link
-                        </NavLink>
-                    </li>
 
                 </ul>
             </nav>
